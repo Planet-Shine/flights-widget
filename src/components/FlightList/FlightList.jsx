7bed1be3c9620ad5a53337d44d6cd272ba89014a
PavelOfficial <pavelofficial@ya.ru>
2017-03-20 17:55:33 +0300

    class name fixes

--- src/components/FlightList/FlightList.jsx
+++ src/components/FlightList/FlightList.jsx
@@ -15,11 +15,11 @@ class FlightList extends Component {
                             departure = new Date(Date.parse(departure));
                             arrival = new Date(Date.parse(arrival));
                             return (<li className="flight-list__item" key={id}>
-                                <span className="flight-list__carrier">{carrier}</span>
-                                <span className="flight-list__flight">{from}—{to}</span><br/>
-                                <span className="flight-list__departure">ВЫЛЕТ: {$date.getDateString(departure)}, <span className="flight-list__day">{$date.getDay(departure)}</span></span><br/>
-                                <span className="flight-list__departure-time">{departure.getHours()}:{departure.getMinutes()}</span>
-                                —<span className="flight-list__arrival-time">{arrival.getHours()}:{arrival.getMinutes()}</span>
+                                <span className="flight-list__item-carrier">{carrier}</span>
+                                <span className="flight-list__item-flight">{from}—{to}</span><br/>
+                                <span className="flight-list__item-departure">ВЫЛЕТ: {$date.getDateString(departure)}, <span className="flight-list__item-day">{$date.getDay(departure)}</span></span><br/>
+                                <span className="flight-list__item-departure-time">{departure.getHours()}:{departure.getMinutes()}</span>
+                                —<span className="flight-list__item-arrival-time">{arrival.getHours()}:{arrival.getMinutes()}</span>
                             </li>);
                         }
                     )}
